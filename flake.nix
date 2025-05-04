7b361fcbc88495adbe0c2d193cc2aae52ff5819b
Rashid J. Almheiri <r.muhairi@pm.me>
2025-05-04 18:40:38 +0400

    darwin init

--- flake.nix
+++ flake.nix
@@ -1,34 +1,25 @@
 {
-  description = ''
-    ae.systems/Risala: Monorepo for Risala, a powerful yet secure messenger for the GCC
-  '';
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.parts = {
+    url = "github:hercules-ci/flake-parts";
+    inputs.nixpkgs-lib.follows = "nixpkgs";
+  };
 
   outputs =
-    inputs@{ parts, systems, ... }:
-    parts.lib.mkFlake { inherit inputs; } {
-      systems = import systems;
+    inputs:
+    inputs.parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "x86_64-linux"
+      ];
+
       imports = [ ];
     };
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    systems.url = "github:nix-systems/default";
-
-    parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
-
-    treefmt = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-
-  nixConfig = {
-    flake-registry = "";
-    show-trace = true;
-
-    experimental-features = "flakes nix-command pipe-operators fetch-closure";
-  };
+  nixConfig.experimental-features = [
+    "flakes"
+    "nix-command"
+    "pipe-operators"
+    "fetch-closure"
+  ];
 }
